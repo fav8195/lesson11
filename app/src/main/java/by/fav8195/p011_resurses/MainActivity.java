2addb73433095ec5dae10f46c3e5cea415b25334
Work <filinkov@u-plast.by>
2020-01-09 16:50:21 +0300

    Создание

---
+++ app/src/main/java/by/fav8195/p011_resurses/MainActivity.java
@@ -0,0 +1,18 @@
+package by.fav8195.p011_resurses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageDrawable(getDrawable(R.drawable.ic_accessibility_grey600_48dp));
+    }
+}
